79d1e5d6e2d98a67d213e9fc4ef2f4b9d720176b
a190884810 <39787887+a190884810@users.noreply.github.com>
2019-05-13 20:00:55 -0500

    Update MainActivity.java
    Change Activity layout

--- FinalProject_ScavengerHunt/app/src/main/java/com/themattburton/cs490/finalproject/MainActivity.java
+++ FinalProject_ScavengerHunt/app/src/main/java/com/themattburton/cs490/finalproject/MainActivity.java
@@ -2,23 +2,65 @@ package com.themattburton.cs490.finalproject;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    Button logIn;
+    EditText username,passwordforemail;
+    private FirebaseAuth mAuth;
+    private String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        username = (EditText) findViewById(R.id.emailAddress);
+        passwordforemail = (EditText) findViewById(R.id.password);
+        logIn = (Button) findViewById(R.id.logIn);
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
 
-        //TODO:  this home screen activity is just a stub, and really needs dressed up
+        //TODO:  this home screen activity is just a stub, and really needs dressed up (done)
     }
 
-    public void startScavengerHunt (View view) {
-        Intent intent = new Intent(this,ScavengerHuntBeginActivity.class);
-        this.startActivity(intent);
+
+    private void signIn() {
+        String email = username.getText().toString();
+        String password = passwordforemail.getText().toString();
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete( Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    startActivity(new Intent(MainActivity.this, ScavengerHuntBeginActivity.class));
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+                    Toast.makeText(MainActivity.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
 }
